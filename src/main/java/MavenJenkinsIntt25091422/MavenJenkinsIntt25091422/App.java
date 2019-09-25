157c3980acd7e27b087d0e3fb40d850cf990cdfa
sai teja <mamidisrisaiteja@gmail.com>
2019-09-25 14:41:06 +0530

    final

---
+++ src/main/java/MavenJenkinsIntt25091422/MavenJenkinsIntt25091422/App.java
@@ -0,0 +1,13 @@
+package MavenJenkinsIntt25091422.MavenJenkinsIntt25091422;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MavenJenkinsIntt25091422/MavenJenkinsIntt25091422/Demo1.java
@@ -0,0 +1,11 @@
+package MavenJenkinsIntt25091422.MavenJenkinsIntt25091422;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void demo() {
+	System.out.println("hai hello world");
+	}
+
+}
